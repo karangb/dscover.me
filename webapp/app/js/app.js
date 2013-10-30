46f286828d6f651f69105fb74e4234692189ae45
Aaron Sananes <aasananes@gmail.com>
2013-10-30 18:44:33 +0000

    Added some animation!

--- webapp/app/js/app.js
+++ webapp/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 (function(window) {
-angular.module('dscover.me', ['ngRoute'])
+angular.module('dscover.me', ['ngRoute', 'ngAnimate'])
   .config(['$routeProvider', function (routeProvider) {
     routeProvider
       .when('/', {
