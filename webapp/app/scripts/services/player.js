fff9ab26c1d175f8ee60286543f8c375e1171792
Aaron Sananes <aasananes@gmail.com>
2013-11-14 13:32:14 +0000

    cleaned up player factory, added injections

--- webapp/app/scripts/services/player.js
+++ webapp/app/scripts/services/player.js
@@ -1,16 +1,11 @@
 'use strict';
 
 angular.module('dscovermeApp')
-  .factory('player', function () {
-    // Service logic
-    // ...
+  .factory('player', function ($http, audio, fetchTracks) {
 
-    var meaningOfLife = 42;
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+
     };
   });
