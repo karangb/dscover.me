4de631eebea1c3176badb9062dd4e87f84145ff2
Aaron Sananes <aasananes@gmail.com>
2013-11-14 14:37:41 +0000

    added prev and next, updated $scope to $rootScope

--- webapp/app/scripts/services/player.js
+++ webapp/app/scripts/services/player.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dscover.me')
-  .factory('player', function ($http, audio, fetchTracks) {
+  .factory('player', function ($rootScope, $http, audio, fetchTracks) {
 
     var player,
         loader,
@@ -14,32 +14,52 @@ angular.module('dscover.me')
       player:player,
       playing: false,
       loader: loader,
+      current: current,
 
-        play: function() {
-          player.loader = true;
-          var hypemId = $scope.tracks[current].hypemId;
-          if(paused === true) {
-            player.loader = false;
-            audio.play();
-            player.playing = true;
+      play: function() {
+        player.loader = true;
+        var hypemId = $rootScope.tracks[player.current].hypemId;
+        if(paused === true) {
+          player.loader = false;
+          audio.play();
+          player.playing = true;
 
-          } else {
-            fetchTracks.mp3(hypemId).success(function(response) { 
-            var mp3url = eval(response);
-            audio.src = mp3url;
-            audio.play();
-            player.playing = true;
-            player.loader = false;
-            });
-          }          
-        },
-        pause: function() {
-          if(player.playing) {
-            audio.pause();
-            player.playing = false
-            paused = true
-          }
-        },
+        } else {
+          fetchTracks.mp3(hypemId).success(function(response) { 
+          var mp3url = eval(response);
+          audio.src = mp3url;
+          audio.play();
+          player.playing = true;
+          player.loader = false;
+          });
+        }          
+      },
+      pause: function() {
+        if(player.playing) {
+          audio.pause();
+          player.playing = false
+          paused = true
+        }
+      },
+      next: function() {
+        paused = false;
+        if ($rootScope.tracks.length > (player.current + 1)) {
+          player.current++;
+          audio.pause();
+        } 
+        else {
+          player.current = 0;
+        }
+         if(player.playing) player.play();     
+      },  
+      prev: function() {
+        paused = false;
+        if (player.current > 0) {
+        player.current--;
+        audio.pause();
+        if(player.playing) player.play();
+        }
+      }
     };
     return player
   });
