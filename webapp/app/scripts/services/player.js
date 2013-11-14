b01c3a9ba6163a498c675cc1e4037874bf32f778
Aaron Sananes <aasananes@gmail.com>
2013-11-14 13:45:12 +0000

    added play function

--- webapp/app/scripts/services/player.js
+++ webapp/app/scripts/services/player.js
@@ -4,12 +4,29 @@ angular.module('dscover.me')
   .factory('player', function ($http, audio, fetchTracks) {
 
     var player,
+        loader,
         paused = false,
         playing = false,
         current = 0;
 
     // Public API here
     return {
-
+        play: function() {
+          loader = true;
+          var hypemId = $scope.tracks[current].hypemId;
+          if(paused === true) {
+            loader = false;
+            audio.play();
+            playing = true;
+          } else {
+            fetchTracks.mp3(hypemId).success(function(response) { 
+            var mp3url = eval(response);
+            audio.src = mp3url;
+            audio.play();
+            playing = true;
+            loader = false;
+            });
+          }          
+        }
     };
   });
