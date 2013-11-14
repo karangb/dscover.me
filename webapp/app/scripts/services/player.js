f7d738dcdc90eecc64aebbd84bef69a22449854f
Aaron Sananes <aasananes@gmail.com>
2013-11-14 13:38:32 +0000

    added player factory variables

--- webapp/app/scripts/services/player.js
+++ webapp/app/scripts/services/player.js
@@ -3,6 +3,10 @@
 angular.module('dscover.me')
   .factory('player', function ($http, audio, fetchTracks) {
 
+    var player,
+        paused = false,
+        playing = false,
+        current = 0;
 
     // Public API here
     return {
