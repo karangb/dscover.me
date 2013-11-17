4e4fded1dff2ac37a7da076bf3ab02091c74aebb
Aaron Sananes <aasananes@gmail.com>
2013-11-17 20:14:38 +0000

    Updates css, animations and overall fixes

--- landing/app/scripts/main.js
+++ landing/app/scripts/main.js
@@ -1,48 +1,45 @@
 console.log('\'Allo \'Allo!');
 
-$(window).scroll(function() {
-  var topOfWindow = $(window).scrollTop();
-  $('.feature-1 .featured-image').each(function(){
-  var imagePos = $(this).offset().top;
 
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("slideUp");
-    }
-  });
-  $('.feature-2 .featured-image').each(function(){
-  var imagePos = $(this).offset().top;
 
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("pulse");
-    }
-  });
-  $('.feature-4 .featured-image, .feature-2 .feature-description').each(function(){
-  var imagePos = $(this).offset().top;
+if ($(window).height() >= 980) {
+    $(".feature").waypoint({
+        triggerOnce: true,
+        handler: function(direction)
+        {
+            if (direction == 'down')
+            {
+                var $this = $(this);
+                $this.addClass("animate");
+            }
+        },
+        offset: '75%'
+    });
+} else {
+    $(".feature").waypoint({
+        triggerOnce: true,
+        handler: function(direction)
+        {
+            if (direction == 'down')
+            {
+                var $this = $(this);
+                $this.addClass("animate");
 
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("slideLeft");
-    }
-  });
-  $('.feature-4 .feature-description, .feature-1 .feature-description').each(function(){
-  var imagePos = $(this).offset().top;
+            }
+        },
+        offset: '50%'
+    });
+    $("#signup").waypoint({
+      handler: function(direction) {
+        $(this).find('.signup-leader').addClass('fade-in');
+      },
+      offset: '50%'
+    });
 
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("slideRight");
-    }
-  });
-  $('.feature-3').each(function(){
-  var imagePos = $(this).offset().top;
-
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("fade-in");
-    }
-  });
-  $('.signup-leader').each(function(){
-  var imagePos = $(this).offset().top;
-
-    if (imagePos < topOfWindow+500) {
-      $(this).addClass("fade-in");
-    }
-  });
-
-});
+    $(".feature-3").waypoint({
+      handler: function(direction) {
+        $(this).addClass("fade-in");
+      },
+      offset: '50%'
+    });
+}
