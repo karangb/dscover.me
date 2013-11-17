929c873560909d1b6be1bdc914ec91c8eaa27e6f
Aaron Sananes <aasananes@gmail.com>
2013-11-17 14:58:08 +0000

    Fixed jquery issue

--- landing/app/scripts/main.js
+++ landing/app/scripts/main.js
@@ -1,10 +1,10 @@
 console.log('\'Allo \'Allo!');
 
 $(window).scroll(function() {
+  var topOfWindow = $(window).scrollTop();
   $('.feature-1 .featured-image').each(function(){
   var imagePos = $(this).offset().top;
 
-  var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
       $(this).addClass("slideUp");
     }
@@ -12,7 +12,6 @@ $(window).scroll(function() {
   $('.feature-2 .featured-image').each(function(){
   var imagePos = $(this).offset().top;
 
-  var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
       $(this).addClass("pulse");
     }
@@ -20,7 +19,6 @@ $(window).scroll(function() {
   $('.feature-4 .featured-image, .feature-2 .feature-description').each(function(){
   var imagePos = $(this).offset().top;
 
-  var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
       $(this).addClass("slideLeft");
     }
@@ -28,17 +26,22 @@ $(window).scroll(function() {
   $('.feature-4 .feature-description, .feature-1 .feature-description').each(function(){
   var imagePos = $(this).offset().top;
 
-  var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
       $(this).addClass("slideRight");
+    }
+  });
+  $('.feature-3').each(function(){
+  var imagePos = $(this).offset().top;
+
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("fade-in");
     }
   });
   $('.signup-leader').each(function(){
   var imagePos = $(this).offset().top;
 
-  var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
-      $(this).addClass("fadeIn");
+      $(this).addClass("fade-in");
     }
   });
 
