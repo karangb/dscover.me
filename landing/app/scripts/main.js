7a3e8756d9fe2eb0d85f67fb8cac9f820522a6ef
Aaron Sananes <aasananes@gmail.com>
2013-11-17 13:20:30 +0000

    Finished landing page, created crazy animations

--- landing/app/scripts/main.js
+++ landing/app/scripts/main.js
@@ -1,1 +1,50 @@
 console.log('\'Allo \'Allo!');
+
+$(window).scroll(function() {
+  $('.feature-1 .featured-image').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("slideUp");
+    }
+  });
+  $('.feature-2 .featured-image').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("pulse");
+    }
+  });
+  $('.feature-4 .featured-image, .feature-2 .feature-description').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("slideLeft");
+    }
+  });
+  $('.feature-4 .feature-description, .feature-1 .feature-description').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("slideRight");
+    }
+  });
+  $('.signup-leader').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("fadeIn");
+    }
+  });
+  $('.learn-more').click( function() {
+    $('#features').ScrollTo({
+    });
+  });
+
+
+});
