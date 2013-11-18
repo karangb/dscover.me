0e63c11c643b2877aba085210ee18a95837eefd5
Aaron Sananes <aasananes@gmail.com>
2013-11-18 18:29:43 +0000

    Major update overall

--- landing/app/scripts/main.js
+++ landing/app/scripts/main.js
@@ -43,3 +43,24 @@ if ($(window).height() >= 980) {
       offset: '50%'
     });
 }
+
+$(function () {
+
+  if (window.devicePixelRatio == 2) {
+
+          var images = $("img");
+
+          // loop through the images and make them hi-res
+          for(var i = 0; i < images.length; i++) {
+
+            // create new image name
+            var imageType = images[i].src.substr(-4);
+            var imageName = images[i].src.substr(0, images[i].src.length - 4);
+            imageName += "@2x" + imageType;
+
+            //rename image
+            images[i].src = imageName;
+          }
+     }
+
+});
