f938ea207926ceaf344badad2f12c6c7c962e81e
Aaron Sananes <aasananes@gmail.com>
2013-12-01 10:12:58 +0000

    Added cookies functions

--- webapp/app/scripts/services/cookies.js
+++ webapp/app/scripts/services/cookies.js
@@ -2,5 +2,17 @@
 
 angular.module('dscover.me')
   .factory('setLogin', function ($cookieStore) {
+    var self = this;
+    return {
+      setUser: function() {
+          $cookieStore.set('loginUser');
+      },
+      lastUser: function() {
+          return $cookieStore.get('loginUser');
+      },
+      addUser: function(user) {
+        $cookieStore.put('loginUser', user);
+      }
 
+    }
   });
